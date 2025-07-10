e67ca7d2af0235b225b50e135243e4c7ee28c43d
kaleddavidmolinaurieta <kaleddavidmolinaurieta@gmail.com>
2025-07-10 20:54:53 +0000

    vista dashboard v1.1

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,8 +1,8 @@
 <x-filament-panels::page>
+@livewire(\App\Filament\Widgets\OrderStatsOverview::class)
 
-    <x-filament-widgets::widgets 
-        :widgets="$this->getWidgets()"
-        :data="['status' => $this->status]"
-    />
-
+{{-- Renderiza el widget de la tabla en un nuevo contenedor, forzándolo a ocupar todo el ancho. --}}
+<div class="mt-6">
+    @livewire(\App\Filament\Widgets\FilteredOrdersTable::class, ['status' => $this->status])
+</div>
 </x-filament-panels::page>
