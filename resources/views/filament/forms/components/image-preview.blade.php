f691bacfae0e01779e48f0775ae52f592e6f3b01
kaleddavidmolinaurieta <kaleddavidmolinaurieta@gmail.com>
2025-08-03 22:07:48 +0000

    prueba v5

--- resources/views/filament/forms/components/image-preview.blade.php
+++ resources/views/filament/forms/components/image-preview.blade.php
@@ -1,7 +1,7 @@
 <div>
     {{-- La variable $id ahora es pasada directamente desde el OrdenResource a través de viewData() --}}
     @if(isset($id) && $id)
-        <img src="{{ route('private-fotos', ['ordenFoto' => $id]) }}"
+        <img src="{{ route('show', ['ordenFoto' => $id]) }}"
              alt="Foto de la orden"
              class="rounded-lg shadow-md object-cover w-full h-40">
     @endif
