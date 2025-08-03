479917a0cad656ff10c506b51caa2f561b400d53
kaleddavidmolinaurieta <kaleddavidmolinaurieta@gmail.com>
2025-08-03 21:56:40 +0000

    prueba v2

--- resources/views/filament/forms/components/image-preview.blade.php
+++ resources/views/filament/forms/components/image-preview.blade.php
@@ -1,19 +1,7 @@
-@php
-    // $getKey() nos da la clave única para el item actual en el que estamos dentro del Repeater.
-    $repeaterItemKey = $getKey();
-    
-    // Construimos la ruta completa al estado del campo 'id' para este item específico.
-    // 'data' es el array que contiene toda la información del formulario en Livewire.
-    $idStatePath = 'data.fotos_existentes.' . $repeaterItemKey . '.id';
-    
-    // Usamos el componente Livewire para obtener el valor (el ID de la foto) de esa ruta específica.
-    $fotoId = $getLivewire()->get($idStatePath);
-@endphp
-
 <div>
-    {{-- Nos aseguramos de que el ID de la foto exista antes de generar la URL --}}
-    @if($fotoId)
-        <img src="{{ route('fotos.show', ['ordenFoto' => $fotoId]) }}"
+    {{-- La variable $id ahora es pasada directamente desde el OrdenResource a través de viewData() --}}
+    @if(isset($id) && $id)
+        <img src="{{ route('fotos.show', ['ordenFoto' => $id]) }}"
              alt="Foto de la orden"
              class="rounded-lg shadow-md object-cover w-full h-40">
     @endif
