5ea8ffc8c82d88be26bedfb77ef3678170d74291
kaleddavidmolinaurieta <kaleddavidmolinaurieta@gmail.com>
2025-08-05 19:15:26 +0000

    fin beta v5 api

--- routes/api.php
+++ routes/api.php
@@ -26,16 +26,24 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
 
     // Rutas para las Órdenes
     Route::get('/orders', [OrderController::class, 'index']);
-    Route::get('/orders/{orden}', [OrderController::class, 'show']);
-    Route::post('/orders/{orden}/accept', [OrderController::class, 'acceptOrder']);
-    Route::post('/orders/{orden}/close', [OrderController::class, 'closeOrder']);
-    Route::post('/orders/{orden}/reject', [OrderController::class, 'rejectOrder']);
+   // Route::get('/orders/{orden}', [OrderController::class, 'show']);
+    //Route::post('/orders/{orden}/accept', [OrderController::class, 'acceptOrder']);
+   // Route::post('/orders/{orden}/close', [OrderController::class, 'closeOrder']);
+    //Route::post('/orders/{orden}/reject', [OrderController::class, 'rejectOrder']);
     //Rutas para usuario
     Route::get('/me', [AuthController::class, 'me']);
     //Rutas para fotos
-    Route::post('/orders/{orden}/update-details', [OrderController::class, 'updateDetails']); 
-    Route::post('/orders/{orden}/upload-photo', [OrderController::class, 'uploadPhoto']);
+    //Route::post('/orders/{orden}/update-details', [OrderController::class, 'updateDetails']); 
+    //Route::post('/orders/{orden}/upload-photo', [OrderController::class, 'uploadPhoto']);
+    //Route::get('/orders/{orden}/photos', [OrderController::class, 'getPhotos']);
+    //Route::get('/private-fotos/{ordenFoto}', [OrderController::class, 'showPhoto']);
+
+    Route::get('/orders/{orden:numero_orden}', [OrderController::class, 'show']);
+    Route::post('/orders/{orden:numero_orden}/accept', [OrderController::class, 'acceptOrder']);
+    Route::post('/orders/{orden:numero_orden}/close', [OrderController::class, 'closeOrder']);
+    Route::post('/orders/{orden:numero_orden}/reject', [OrderController::class, 'rejectOrder']);
+    Route::post('/orders/{orden:numero_orden}/update-details', [OrderController::class, 'updateDetails']); 
+    Route::post('/orders/{orden:numero_orden}/upload-photo', [OrderController::class, 'uploadPhoto']);
     Route::get('/orders/{orden:numero_orden}/photos', [OrderController::class, 'getPhotos']);
-    Route::get('/private-fotos/{ordenFoto}', [OrderController::class, 'showPhoto']);
 
 });
